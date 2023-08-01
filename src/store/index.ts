7ca5e24b3c39c39cac8f534bb071710339dfa97e
vladislav-savko <vlad.savko21@gmail.com>
2023-08-01 04:14:04 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { contactApi } from './web/contact.api'
+import { webApi } from './web/web.api'
 
 export const store = configureStore({
   reducer: {
     [contactApi.reducerPath]: contactApi.reducer,
+    [webApi.reducerPath]: webApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(contactApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(contactApi.middleware, webApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
